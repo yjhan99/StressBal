1394a6a8b8116c1648b1312ae121af0861b72b7c
yjhan99 <yjhan99@kaist.ac.kr>
2022-08-23 23:07:11 +0900

    V2 추가 및 Intervention UI 구현

--- Stress Intervention App V2/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm2/activities/InterventionActivity2.kt
+++ Stress Intervention App V2/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm2/activities/InterventionActivity2.kt
@@ -1,5 +1,6 @@
 package com.garmin.android.apps.connectiq.sample.comm2.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
